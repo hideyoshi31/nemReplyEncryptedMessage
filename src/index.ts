0fd70be2d1fb56bd1e29ac14da1b9827fbe6feb8
Toshiya Matsukawa <matsukawa5955@gmail.com>
2019-01-26 22:47:01 +0900

    未承認トランザクションコメントアウト

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,6 @@ nemwrap.incomingTransactions(ADRESS)
     })
   })
 */
-
 export const nemReplyEncryptedMessage = functions.https.onRequest(async(req, res) => {
   try {
     //入金履歴
@@ -28,15 +27,15 @@ export const nemReplyEncryptedMessage = functions.https.onRequest(async(req, res
     //出金履歴
     const r3:any = await nemwrap.getOutgoingTransactions(ADRESS)
     //未承認トランザクション
-    const r4:any = await nemwrap.getUnconfirmedTransactions(ADRESS)
+    /*const r4:any = await nemwrap.getUnconfirmedTransactions(ADRESS)
     //3と4の連結
-    const r5 = Object.assign(r3, r4);
-    //未出金かつ入金済み
+    const r5 = Object.assign(r3, r4)*/
+    //入金済みかつ未送信アドレス抽出
     const r6: any[] = []
     let counter: number =0
     for(let i = 0; i < r2.length; i++){
-      for(let j = 0; j<r5.length; j++){
-        if(counter===0 && r2[i].signer.address.value === r5[j].recipient.value){
+      for(let j = 0; j<r3.length; j++){
+        if(counter===0 && r2[i].signer.address.value === r3[j].recipient.value){
           counter++
         }
       }
@@ -46,12 +45,14 @@ export const nemReplyEncryptedMessage = functions.https.onRequest(async(req, res
       counter = 0
     }
     //重複削除
-    const r7 = r6.filter(function (x, i, self) {
-      return self.indexOf(x) === i;
-    });
+    const r7 = r6.filter((x:any, i, self) => self.indexOf(x) === i)
+
     r7.forEach(async(address:any) => {
-      const pbkey = await nemwrap.getPublickKey(address.address)
-      const result = await nemwrap.sendEncryptMessage(pbkey,address.address,SEND_MESSAGE)
+      //公開鍵取得
+      const pbkey = await nemwrap.getPublickKey(address)
+      console.log(address)
+      //メッセージ送信
+      const result = await nemwrap.sendEncryptMessage(pbkey,address,SEND_MESSAGE)
       console.log(result)
     })
   }catch(error) {
